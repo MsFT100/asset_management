297413113ba5f264501c96c3482f87fc8272515e
Nelvine <nelvine@localhost.localdomain>
2023-07-05 17:04:07 +0300

    made the changes asked

--- department/forms.py
+++ department/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Department
+from branch.models import Branch
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Field
 
@@ -12,14 +13,16 @@ class DepartmentForm(forms.ModelForm):
             'name',
             'comment',
             'department_active',
+            'branch',
             Submit('submit', 'Save')
         )
 
     class Meta:
         model = Department
-        fields = ['name', 'comment', 'department_active']
+        fields = ['name', 'comment', 'department_active', 'branch']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'comment': forms.TextInput(attrs={'class': 'form-control'}),
+            'branch': forms.Select(attrs={'class': 'form-control'}),
             'department_active': forms.CheckboxInput(attrs={'class': 'form-control'}),
         }
