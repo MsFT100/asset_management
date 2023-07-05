a2555987cd558273b0b54d3e3d4ffb84441db529
Nelvine <nelvine@localhost.localdomain>
2023-07-05 10:21:44 +0300

    removed space between IT and A

--- itasset/settings.py
+++ itasset/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # IT Asset apps
+    # ITAsset apps
     "company",
     "branch",
     "employee",
